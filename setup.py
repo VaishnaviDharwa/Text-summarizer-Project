98e595f28546e6a6304a1e2053e976904a62bc2c
VaishnaviDharwa <vaishnavid1302@gmail.com>
2026-03-02 22:09:01 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md","r",encoding="utf-8")as f:
+    long-description = f.read()
+
+
+    _version_="0.0.0"
+
+    REPO_NAME="Text-Summarizer-Project"
+    AUTHOR_USER_NAME = "VaishnaviDharwa"
+    SRC_REPO ="textsummarizer"
+    AUTHOR_EMAIL="vaishnavid1302@gmail.com"
+
+
+
+    setuptools.setup(
+        name=SRC_REPO,
+        version=_version_,
+        author=AUTHOR_USER_NAME,
+        author_email=AUTHOR_EMAIL,
+        description="A small python package for NLP app",
+        Long_description=long_description,
+        Long_description_content="text/markdown",
+        url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+        project_urls={
+            "Bug Tracker":f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAMR}/issues",
+        },
+        package_dir={"": "src"},
+        packages=setuptools.find_packages(where="src")
+    )
